90f79cc3b467cf7f4aab3f34d76bc6a7e719b450
hotio <hotio@protonmail.com>
2019-11-14 13:09:13 +0100

    update

--- update.sh
+++ update.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-###################
 version=$(curl -fsSL "https://services.lidarr.audio/v1/update/nightly/changes?os=linux" | jq -r .[0].version)
-app=LIDARR
-###################
-
-location=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-cd "${location}" || exit 1
-
-find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG ${app}_VERSION=.*$/ARG ${app}_VERSION=${version}/g" {} \;
+find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG LIDARR_VERSION=.*$/ARG LIDARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
-
-if [[ -n "$(git status --untracked-files=no --porcelain)" ]]; then
-    git add ./*.Dockerfile
-    git add ./.drone.yml
-    git commit -m "Updated version to: ${version}"
-    echo "Updated version to: ${version}"
-else
-    echo "Version is still: ${version}"
-fi
+echo "##[set-output name=version;]${version}"
