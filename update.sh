0a2f836273807e9640df25bd1a483301283bf524
hotio <hotio@protonmail.com>
2019-11-16 10:06:54 +0100

    [CI SKIP] add failsafe

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 version=$(curl -fsSL "https://services.lidarr.audio/v1/update/nightly/changes?os=linux" | jq -r .[0].version)
+[[ -z ${version} ]] && exit
 find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG LIDARR_VERSION=.*$/ARG LIDARR_VERSION=${version}/g" {} \;
 sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
 echo "##[set-output name=version;]${version}"
