89ebec9eed130a87ca9aa1e8ed2bcbd60bf4a005
hotio <26902309+hotio@users.noreply.github.com>
2020-04-26 16:33:43 +0200

    [CI SKIP]

--- update.sh
+++ update.sh
@@ -44,7 +44,7 @@ else
     branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/lidarr/lidarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select(.head.repo.full_name == "lidarr/Lidarr") | .head.ref' | tail -n 1)
     version=$(curl -fsSL "https://services.lidarr.audio/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
-    [[ ${version} == null ]] && exit 0
+    [[ ${version} == "null" ]] && exit 0
     sed -i "s/{LIDARR_VERSION=[^}]*}/{LIDARR_VERSION=${version}}/g" .drone.yml
     sed -i "s/{LIDARR_BRANCH=[^}]*}/{LIDARR_BRANCH=${branch}}/g" .drone.yml
     echo "##[set-output name=version;]${branch}-${version}"
