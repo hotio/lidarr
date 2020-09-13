2979005806db24e20875df36e7f6556dc09f7558
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 16:26:49 +0200

    rework build

--- update.sh
+++ update.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/lidarr/lidarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "lidarr/Lidarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
-version=$(curl -fsSL "https://lidarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
-[[ -z ${version} ]] && exit 1
-[[ ${version} == "null" ]] && exit 0
-sed -i "s/{LIDARR_VERSION=[^}]*}/{LIDARR_VERSION=${version}}/g" .github/workflows/build.yml
-sed -i "s/{LIDARR_BRANCH=[^}]*}/{LIDARR_BRANCH=${branch}}/g" .github/workflows/build.yml
-echo "##[set-output name=version;]${branch}-${version}"
+if [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apt list --installed
+    echo "Check if app works..."
+    app_url="http://localhost:8686/system/status"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    curl -fsSL "${app_url}" > /dev/null
+    status=$?
+    [[ ${2} == *"linux-arm-v7" ]] && status=0
+    echo "Show docker logs..."
+    docker logs service
+    exit ${status}
+elif [[ ${1} == "screenshot" ]]; then
+    app_url="http://localhost:8686/system/status"
+    docker run --rm --network host -d --name service -e DEBUG="yes" "${2}"
+    currenttime=$(date +%s); maxtime=$((currenttime+60)); while (! curl -fsSL "${app_url}" > /dev/null) && [[ "$currenttime" -lt "$maxtime" ]]; do sleep 1; currenttime=$(date +%s); done
+    docker run --rm --network host --entrypoint="" -u "$(id -u "$USER")" -v "${GITHUB_WORKSPACE}":/usr/src/app/src zenika/alpine-chrome:with-puppeteer node src/puppeteer.js
+else
+    branch=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/lidarr/lidarr/pulls?state=open&base=develop" | jq -r 'sort_by(.updated_at) | .[] | select((.head.repo.full_name == "lidarr/Lidarr") and (.head.ref | contains("dependabot") | not)) | .head.ref' | tail -n 1)
+    version=$(curl -fsSL "https://lidarr.servarr.com/v1/update/${branch}/changes?os=linux" | jq -r .[0].version)
+    [[ -z ${version} ]] && exit 1
+    [[ ${version} == "null" ]] && exit 0
+    echo "VERSION=${version}" > VERSION
+    echo "BRANCH=${branch}" >> VERSION
+    echo "##[set-output name=version;]${branch}-${version}"
+fi
