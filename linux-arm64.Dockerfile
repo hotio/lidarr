649f952c78624e9b912c9cdb57a6a41a4b4dd14c
mrhotio <26902309+mrhotio@users.noreply.github.com>
2021-10-01 19:24:11 +0200

    switch to musl

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -1,27 +1,19 @@
-FROM ghcr.io/hotio/base@sha256:c3c9b5bcb8a6902be93f3e33ff9156dc0e6e766c6ca1acdb48d81e51c7d8d30e
-
-ARG DEBIAN_FRONTEND="noninteractive"
+FROM ghcr.io/hotio/base@sha256:05ed569c200448e11b1fb72bbd9609abef36f9d64f3174a61d908a89fe44c75c
 
 EXPOSE 8686
 
-# install packages
-RUN apt update && \
-    apt install -y --no-install-recommends --no-install-suggests \
-        libchromaprint-tools libicu66 && \
-# clean up
-    apt autoremove -y && \
-    apt clean && \
-    rm -rf /tmp/* /var/lib/apt/lists/* /var/tmp/*
+RUN apk add --no-cache libintl sqlite-libs icu-libs && \
+    apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/edge/community chromaprint
 
 ARG VERSION
 ARG PACKAGE_VERSION=${VERSION}
 
 # install app
 RUN mkdir "${APP_DIR}/bin" && \
-    curl -fsSL "https://lidarr.servarr.com/v1/update/develop/updatefile?version=${VERSION}&os=linux&runtime=netcore&arch=arm64" | tar xzf - -C "${APP_DIR}/bin" --strip-components=1 && \
+    curl -fsSL "https://lidarr.servarr.com/v1/update/develop/updatefile?version=${VERSION}&os=linuxmusl&runtime=netcore&arch=arm64" | tar xzf - -C "${APP_DIR}/bin" --strip-components=1 && \
     rm -rf "${APP_DIR}/bin/Lidarr.Update" && \
     rm -f "${APP_DIR}/bin/fpcalc" && \
-    echo "PackageVersion=${PACKAGE_VERSION}\nPackageAuthor=[hotio](https://github.com/hotio)\nUpdateMethod=Docker\nBranch=develop" > "${APP_DIR}/package_info" && \
+    echo -e "PackageVersion=${PACKAGE_VERSION}\nPackageAuthor=[hotio](https://github.com/hotio)\nUpdateMethod=Docker\nBranch=develop" > "${APP_DIR}/package_info" && \
     chmod -R u=rwX,go=rX "${APP_DIR}"
 
 COPY root/ /
--- update.sh
+++ update.sh
@@ -3,15 +3,14 @@
 if [[ ${1} == "checkdigests" ]]; then
     export DOCKER_CLI_EXPERIMENTAL=enabled
     image="ghcr.io/hotio/base"
-    tag="focal"
+    tag="alpine"
     manifest=$(docker manifest inspect ${image}:${tag})
     [[ -z ${manifest} ]] && exit 1
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
-    digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux" and .platform.variant == "v7").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
 elif [[ ${1} == "tests" ]]; then
     echo "List installed packages..."
-    docker run --rm --entrypoint="" "${2}" apt list --installed
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
     echo "Check if app works..."
     app_url="http://localhost:8686/system/status"
     docker run --network host -d --name service "${2}"
@@ -29,7 +28,7 @@ elif [[ ${1} == "screenshot" ]]; then
     docker run --rm --network host --entrypoint="" -u "$(id -u "$USER")" -v "${GITHUB_WORKSPACE}":/usr/src/app/src zenika/alpine-chrome:with-puppeteer node src/puppeteer.js
     exit 0
 else
-    version=$(curl -fsSL "https://lidarr.servarr.com/v1/update/develop/changes?os=linux&runtime=netcore&arch=x64" | jq -r .[0].version)
+    version=$(curl -fsSL "https://lidarr.servarr.com/v1/update/develop/changes?os=linuxmusl&runtime=netcore&arch=x64" | jq -r .[0].version)
     [[ -z ${version} ]] && exit 1
     [[ ${version} == "null" ]] && exit 0
     echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
